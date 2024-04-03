45ccd3bac346d2f510952b730a6c8f645c975f0e
Ali Hassan <sydhasn786@gmail.com>
2024-04-03 21:00:05 +0500

    initial project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  //apply to all below extension files to tailwind
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
